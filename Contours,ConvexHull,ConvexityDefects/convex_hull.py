df81d55a481213bd1041d20ea4d229ac2ade5869
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-09-29 13:27:37 +0300

    Convex Hull code has been edited.

--- Contours,ConvexHull,ConvexityDefects/convex_hull.py
+++ Contours,ConvexHull,ConvexityDefects/convex_hull.py
@@ -10,14 +10,11 @@ ret, thresh = cv2.threshold(blur, 40, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 hull = []
+bg = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
 
 for i in range(len(contours)):
     # when false returns the indices of the values from convexHull
     hull.append(cv2.convexHull(contours[i], False))
-
-bg = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
-
-for i in range(len(contours)):
     cv2.drawContours(bg, contours, i, (255, 0, 0), 3, 8)
     cv2.drawContours(bg, hull, i, (0, 255, 0), 1, 8)
 
