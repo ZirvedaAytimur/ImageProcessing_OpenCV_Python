ee8c0dd1e487c1908d873047972b27624c4fc8f1
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-10-04 14:40:58 +0300

    Mouse Event added.

---
+++ Exercises/mouse_event.py
@@ -0,0 +1,32 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+circles = []
+
+
+def mouse(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        circles.append((x, y))
+
+
+cv2.namedWindow("Frame")
+cv2.setMouseCallback("Frame", mouse)
+
+while 1:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    for center in circles:
+        cv2.circle(frame, center, 20, (255, 0, 0), -1)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    # clear change on screen
+    elif key == ord("h"):
+        circles = []
+
+cap.release()
+cv2.destroyAllWindows()
