f4107afffd5f30d6582729e7f3753bec5321e1d5
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-10-21 17:37:11 +0300

    Eye Detection on Webcam added.

---
+++ EyeDetection/EyeDetectionWebcam/eye_detection_webcam.py
@@ -0,0 +1,32 @@
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+eye_cascade = cv2.CascadeClassifier("eye.xml")
+
+while True:
+    ret, frame = vid.read()
+    frame = cv2.flip(frame, 1)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 7)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 70, 80), 3)
+
+    roi_frame = frame[y: y + h, x: x + w]
+    roi_gray = gray[y: y + h, x: x + w]
+
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_frame, (ex, ey), (ex + ew, ey + eh), (0, 90, 75), 2)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(5) & 0xFF == ord("q"):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
