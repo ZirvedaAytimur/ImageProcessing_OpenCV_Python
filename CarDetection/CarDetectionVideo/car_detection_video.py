ecb6306ec017171300a962fbc1337b5c09fbc9bc
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-10-22 21:03:03 +0300

    Car Detection on Video added.

---
+++ CarDetection/CarDetectionVideo/car_detection_video.py
@@ -0,0 +1,24 @@
+import cv2
+
+vid = cv2.VideoCapture("car.mp4")
+car_cascade = cv2.CascadeClassifier("car.xml")
+
+while True:
+    ret, frame = vid.read()
+    frame = cv2.resize(frame, (640, 360))
+    if ret is False:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cars = car_cascade.detectMultiScale(gray, 1.2, 4)
+
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(5) & 0xFF == ord("q"):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
