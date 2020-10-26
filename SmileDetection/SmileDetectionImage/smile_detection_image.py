5d303f1c1a718f313f4147f8fb93e70038d589cf
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-10-26 11:04:05 +0300

    Smile Detection on Image added.

---
+++ SmileDetection/SmileDetectionImage/smile_detection_image.py
@@ -0,0 +1,23 @@
+import cv2
+
+img = cv2.imread("smile.jpg")
+face_cascade = cv2.CascadeClassifier("frontalface.xml")
+smile_cascade = cv2.CascadeClassifier("smile.xml")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+    roi_image = img[y:y + h, x:x + w]
+    roi_gray = gray[y:y + h, x:x + w]
+
+    smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 5)
+
+    for (sx, sy, sw, sh) in smiles:
+        cv2.rectangle(roi_image, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 2)
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
